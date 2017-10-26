d63a0da0f400a895e00a4350be997de8ef1968f7
Outrider99 <outrider.w@gmail.com>
2017-10-26 11:50:30 -0600

    Initial commit -- Updated .gitignore, ordered files

---
+++ Wyatt_Miller_CSP/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wyatt_Miller_CSP
+//
+//  Created by Miller, Wyatt on 10/26/17.
+//  Copyright © 2017 Miller, Wyatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
