577b7c85a5fdeca443ac653c0363ae93053ef0c4
Outrider99 <outrider.w@gmail.com>
2017-10-26 12:09:37 -0600

    Added ViewControllers 1-7 for a total of 8

---
+++ Wyatt_Miller_CSP/Controller/ModelViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ModelViewController.swift
+//  Wyatt_Miller_CSP
+//
+//  Created by Miller, Wyatt on 10/26/17.
+//  Copyright © 2017 Miller, Wyatt. All rights reserved.
+//
+
+import Foundation
