56f337b759fad14c65692d93106e2f475f01e31b
Outrider99 <outrider.w@gmail.com>
2017-11-14 11:54:08 -0700

    Finished labels

--- Wyatt_Miller_CSP/Controller/GlobalImpactViewController.swift
+++ Wyatt_Miller_CSP/Controller/GlobalImpactViewController.swift
@@ -14,11 +14,17 @@ public class GlobalImpactViewController: UIViewController {
     
     @IBOutlet weak var ImpactPhoto: UIImageView!
     @IBOutlet weak var ImpactLabel: UILabel!
+    @IBOutlet weak var ImpactLabel2: UILabel!
+    @IBOutlet weak var ImpactLabel3: UILabel!
     
     override public func viewDidLoad() {
         super.viewDidLoad()
 
         ImpactPhoto.image = UIImage(named: "Notes")
+        ImpactLabel.text = "This app would contain information about nuclear energy that is presented after a short survey on their knowledge."
+        ImpactLabel2.text = "Nuclear energy when handled properly is much cleaner then many other forms of energy generation, the public needs to understand its benefits."
+        ImpactLabel3.text = "I live in the world that is pressed with issues about energy generation. This doesnt have to be one of them."
+        
     }
 
     override public func didReceiveMemoryWarning() {
