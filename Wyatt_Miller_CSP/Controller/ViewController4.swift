63e974d0f60dd3e42a42409fa0d9b94f12c6ac38
Outrider99 <outrider.w@gmail.com>
2017-11-27 12:35:39 -0700

    Swipe integration

--- Wyatt_Miller_CSP/Controller/ViewController4.swift
+++ Wyatt_Miller_CSP/Controller/ViewController4.swift
@@ -5,31 +5,91 @@
 //  Created by Miller, Wyatt on 10/26/17.
 //  Copyright © 2017 Miller, Wyatt. All rights reserved.
 //
+//Astracton
 
 import UIKit
 
 public class ViewController4: UIViewController {
 
-    override public func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override public func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+    {
+        private (set) lazy var orderedAbstractionViews : [UIViewController] =
+        {
+            return [
+                self.newAbstractionViewController(abstractionLevel: "Block"),
+                self.newAbstractionViewController(abstractionLevel: "Java"),
+                self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+                self.newAbstractionViewController(abstractionLevel: "Binary"),
+                self.newAbstractionViewController(abstractionLevel: "AndGate"),
+        ]
+        }()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func newAbstractionViewController(abstractionLevel: String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
     }
-    */
+        
+        override public func viewDidLoad()
+        {
+            super.viewDidLoad()
+            dataSource = self
+            
+            if let firstViewController = orderedAbstractionViews.first
+            {
+                setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+            }
+        }
+
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore
+        viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
+        
+        public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter
+            viewController: UIViewController) -> UIViewController?
+        {
+            guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+                else
+            {
+                return nil
+            }
+            
+            let nextIndex = viewControllerIndex - 1
+            
+            guard nextIndex >= 0
+                else
+            {
+                return orderedAbstractionViews.first
+            }
+            
+            guard orderedAbstractionViews.count > nextIndex
+                else
+            {
+                return nil
+            }
+            
+            return orderedAbstractionViews[nextIndex]
+        }
 
 }
