18074145124aa7b69ffef15bcbd4b11c7601560a
Outrider99 <outrider.w@gmail.com>
2017-12-07 12:54:11 -0700

    made artcell

---
+++ Wyatt_Miller_CSP/Controller/ArtCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArtCell.swift
+//  Wyatt_Miller_CSP
+//
+//  Created by Miller, Wyatt on 12/7/17.
+//  Copyright © 2017 Miller, Wyatt. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageVw: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+    
+
+}
