d5f0a054e62e7424430ee71ed0d4afb247527870
Outrider99 <outrider.w@gmail.com>
2018-01-09 11:51:09 -0700

    Added files for data view controlle

---
+++ Wyatt_Miller_CSP/Controller/DataView/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  Wyatt_Miller_CSP
+//
+//  Created by Miller, Wyatt on 1/9/18.
+//  Copyright © 2018 Miller, Wyatt. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Wyatt_Miller_CSP/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  Wyatt_Miller_CSP
+//
+//  Created by Miller, Wyatt on 1/9/18.
+//  Copyright © 2018 Miller, Wyatt. All rights reserved.
+//
+
+import Foundation
